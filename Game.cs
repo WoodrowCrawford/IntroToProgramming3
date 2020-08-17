ee3414473b48a1077b79065e888748929bbed378
WoodrowCrawford <69813670+WoodrowCrawford@users.noreply.github.com>
2020-08-17 12:41:09 -0500

    Initial commit

---
+++ Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Game
+    {
+        public void Run()
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a new instance of a Game
+            Game game = new Game();
+            //Run the Game
+            game.Run();
+            //Wait before closing
+            Console.ReadKey();
+        }
+    }
+}
