2bd98b1d0ed4a8a8e110229c07c3e7523ffd6c19
Woodrow Crawford <woodrowcrawford03@gmail.com>
2020-08-19 00:30:58 -0500

    Added code
    added just healed and name

--- Game.cs
+++ Game.cs
@@ -8,6 +8,18 @@ namespace HelloWorld
     {
         public void Run()
         {
+            float health = 100.0f;
+            float healthRegen = 20;
+            string name = Console.ReadLine();
+            bool maxLevelReached = false;
+            int maxLevel = 100;
+            int level = 1;
+            bool ready = true;
+            Console.WriteLine(health);
+            health = health + healthRegen;
+            Console.Write( name + " just healed " + healthRegen);
+
+
 
         }
     }
